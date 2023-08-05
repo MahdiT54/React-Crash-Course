dab26c9586ae2b3acd58a9a7b1c4ebaad43933d4
Mahdi <mahditanzim2@gmail.com>
2023-08-04 20:30:19 -0400

    new dir

---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import './Todo.css'
+
+function Todo() {
+  return (
+    <div className='todo'>
+      <h2>Finish Front-end Simplified</h2>
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default Todo;
